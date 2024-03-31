e53e412cb4549cae677f5043361f40d0c5d95e95
Jozhin-s-Bazhin <rbezroutchko@gmail.com>
2024-03-31 12:42:35 +0200

    Cleaned up the cli client

--- src/game_setting_manager/gsm_cli.py
+++ src/game_setting_manager/gsm_cli.py
@@ -2,25 +2,24 @@ import argparse
 from game_setting_manager import save_and_load
 
 
+def add_common_args(subparser):
+    """Adds common argument to a given subparser (parser_save and parser_load)"""
+    subparser.add_argument('game_name', help='Name of the game')
+    subparser.add_argument('profile_name', help='Name of the profile')
+    subparser.add_argument('--data-path', help='Path to saved data, mostly intended for testing', default=save_and_load.DEFAULT_DATA_PATH)
+
 # Create a parser to parse command-line arguments
 parser = argparse.ArgumentParser()
 subparsers = parser.add_subparsers(dest="command")
 
-# Create a parser for the "save" command
+# Save parser
 parser_save = subparsers.add_parser('save', help='Save game settings to a profile')
-parser_save.add_argument('game_name', help='Name of the game')
-parser_save.add_argument('profile_name', help='Name of the profile') 
 parser_save.add_argument('--path', nargs='+', help='Path to configuration files of the game', default=None)  # Use "--path /path/to/config/file" if the path of the game isn't saved already
-parser_save.add_argument('--data-path', help='Path to saved data, mostly intended for testing', default=save_and_load.DEFAULT_DATA_PATH)
-parser_save.add_argument('--overwrite-saved-profiles', help='Whether to overwrite saved profiles if they already exist', default='ask')
+add_common_args(parser_save)
 
-# Create a parser for the "load" command
+# Load parser
 parser_load = subparsers.add_parser('load', help='Load game settings from a profile')
-parser_load.add_argument('game_name', help='Name of the game')
-parser_load.add_argument('profile_name', help='Name of the profile')
-# --path isn't needed because the game must be saved already anyway. If it isn't the program will throw an error
-parser_load.add_argument('--data-path', help='Path to saved data, mostly intended for testing', default=save_and_load.DEFAULT_DATA_PATH)
-# --overwrite-saved-profiles isn't needed either because this doesn't modify the data directory in any way
+add_common_args(parser_load)
 
 # Main function
 def main():
@@ -28,16 +27,12 @@ def main():
     if args.command == 'save':
         try:
             save_and_load.save_profile(args.game_name, args.profile_name, args.path, args.data_path, args.overwrite_saved_profiles)
-        except save_and_load.PathError as e:
-            print(e.message)
-        except save_and_load.DataError as e:
+        except ( save_and_load.PathError, save_and_load.DataError ) as e:
             print(e.message)
     elif args.command == 'load':
         try:
             save_and_load.load_profile(args.game_name, args.profile_name, args.data_path)
-        except save_and_load.PathError as e:
-            print(e.message)
-        except save_and_load.DataError as e:
+        except ( save_and_load.PathError, save_and_load.DataError ) as e:
             print(e.message)
     else:
         parser.print_help()
